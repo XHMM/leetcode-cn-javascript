eb71abe6ca24b33c7c2592cc9e0434aa659aff03
xhmm <super.xhmm@gmail.com>
2019-07-20 16:49:05 +0800

    add BF

---
+++ BruteForce.h
@@ -0,0 +1,55 @@
+//
+// Created by xh on 2019/7/20.
+//
+#ifndef ALGORITHM_BRUTEFORCE_H
+#define ALGORITHM_BRUTEFORCE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+class BruteForce {
+private:
+  static int bruteForce(const std::string& a, const std::string& b) {
+    for(int m = 0; m<a.length() - b.length() + 1; m++) {
+      unsigned int count = 0;
+      for(int n =0; n< b.length(); n++) {
+        if(a.at(m + count) != b.at(n)) break;
+        else {
+          count++;
+        }
+      }
+      if(count == b.length()) return m;
+    }
+    return -1;
+/*  // while loop
+    unsigned int i = 0;
+    unsigned int j = 0;
+    while (i < a.length() && j < b.length()) {
+      if (a[i] == b[j]) {
+        i++;
+        j++;
+      } else {
+        i++;
+        j = 0;
+      }
+    }
+    if (j == b.length()) {
+      return i - b.length();
+    }
+    return -1;*/
+  }
+
+public:
+  static void test() {
+    std::string str = "abcdABCD EFaadfk32!@#34flasf dafe";
+    std::vector<std::string> subs = {
+        "abcde", "ABCD EF", "fe", "!@#", "asf dafe", "afea"
+    };
+    for(const auto& sub : subs) {
+      std::cout << BruteForce::bruteForce(str, sub) << std::endl;
+    }
+  }
+};
+
+#endif //ALGORITHM_BRUTEFORCE_H
